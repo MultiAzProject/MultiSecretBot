ab01c0d2d80d18502156eba73f14819d43d4370d
CahildiBrat <99288895+CahildiBrat@users.noreply.github.com>
2022-10-28 21:38:04 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 class Config:
-    session_name = "asdasdasdasdasd" #Pyrogram v2 string lazimdir. python3 gen.py yazaraq ala bilersiniz
+    session_name = "asdasdasdasdasd" #Pyrogram v1.3.6 string lazimdir. python3 gen.py yazaraq ala bilersiniz
